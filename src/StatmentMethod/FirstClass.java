5afebb6cd9764008e53df1d29fba1ca18f24e305
choubeyshubham <shubhamchoubey96@gmail.com>
2025-03-06 10:46:20 +0530

    06 commit

---
+++ src/StatmentMethod/FirstClass.java
@@ -0,0 +1,35 @@
+package StatmentMethod;
+
+public class FirstClass {
+    /*
+
+
+     */
+    public static void main(String[] args) {
+
+        int myVariable=50;
+
+        myVariable++;
+        myVariable--;
+
+
+        System.out.println("This is a test");
+
+        System.out.println("This is "+
+                "another"+
+                " still more ");
+
+        int anotherVariable=60;
+        myVariable--;
+        System.out.println("myVariable ="+myVariable);
+
+        if (myVariable == 0) {
+            System.out.println("It's is not 0");
+        }
+
+
+
+    }
+
+
+}
